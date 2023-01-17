bad4f11cda6813fd9a1717f8037b6359708a8758
amrizalnahar <zalamri26@gmail.com>
2023-01-17 20:38:25 +0700

    update darkmode dan perbaikan fitur

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['index.html'],
+  darkMode: 'class',
   theme: {
     container: {
       center: true,
-      padding: '20px',
+      padding: '50px',
     },
     extend: {
       colors: {
-        primary: '#be123c',
+        primary: '#475569',
         dark: '#0f172a',
         secondary: '#64748b'
       },
